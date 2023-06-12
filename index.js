9efb40713a82bf367c9b6b2a2674cf7025b11f79
“chomp” <chom@odds.team>
2023-06-12 17:49:40 +0700

    Than

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello mame")
+
+var RSSParser = require('rss-parser');
+
+const feedUrl = "http://feeds.bbci.co.uk/news/rss.xml";
+
+const parse = async (url) => {
+    const feed = await new RSSParser().parseURL(url);
+
+    console.log(feed.title);
+
+    feed.items.forEach(item => {
+        console.log(`${item.title} - ${item.link}\n${item.contentSnippet}\n\n`);
+    });
+};
+
+console.log("Parsing " + feedUrl);
+
+parse(feedUrl);
